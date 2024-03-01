0d7002868c39d0287c05138a3b34962a5f4a87e5
Fernando Maleiane <maleianefernando3@gmail.com>
2024-03-01 07:06:20 +0200

    non importat upgrades to "Menu.java"

--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -5,14 +5,18 @@ import java.awt.Font;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
-
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 import gui.util.Style;
 
 
 public class Menu extends JPanel {
 	private JButton estudante_cadastrado, funcionario_cadastrado, cadastrar, turmas; 
-	
+	private JMenuBar jmenu = new JMenuBar();
+	private JMenu estudante;
+	private JMenuItem[] est_item;
 	Menu(){
 		this.setLayout(Style.flow_center);
 		this.setBackground(Style.blue);
@@ -26,6 +30,7 @@ public class Menu extends JPanel {
 		this.validate();
 	}
 	
+	//setting the buttons
 	private void botoes(JButton btn, String btn_txt, Color bg, Color fg, Font ft){
 		btn = new JButton(btn_txt);
 		btn.setFont(ft);
@@ -35,6 +40,29 @@ public class Menu extends JPanel {
 		//btn.setPreferredSize(new Dimension(375, 50));
 		
 		this.add(btn);
+	}
+	
+	//setting the menu bar
+	public void menu_bar(javax.swing.JFrame jf){
+		jmenu(estudante, "Estudante", est_item, Style.menu_font);
+		
+		jf.setJMenuBar(jmenu);
+	}
+	
+	
+	//setting the menus
+//	est_item = new JMenuItem[3];
+	
+	private void jmenu(JMenu jm, String jm_txt, JMenuItem[] jm_item, Font ft){
+//		jm = new JMenu(jm_txt);
+//		jm.setFont(ft);
+//		
+//		for(int i =0; i < jm_item.length(); i++){
+//			jm_item[i] = new JMenuItem("adc");
+//			
+//			jm_item[i].setFont(ft);
+//			jm.setMenuItem(jm_item[i]);
+//		}
 	}
 }
 
