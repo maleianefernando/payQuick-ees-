31540d6cc8ab46ef1d16a0d29a124f3a26f25c54
Fernando Maleiane <maleianefernando3@gmail.com>
2024-04-05 16:02:17 +0200

    improve the form

--- src/gui/estudante/CadastrarEstudante.java
+++ src/gui/estudante/CadastrarEstudante.java
@@ -40,11 +40,14 @@ public class CadastrarEstudante extends JPanel implements ActionListener{
 	JPanel title_panel = new JPanel(Style.flow_center);
 	
 	//Items to the forms JLabel
-	String[] form_text = new String[] {"Nome Completo", "Morada", "Bairro", "Email", "Data de nascimento", "Numero de celular", "Sexo", "Contacto de emergência", "Nome Completo", "Numero de Celular    (+258)", "nivel"};
+	String[] form_text = new String[] {"Nome Completo", "Morada", "Bairro", "Email", "Data de nascimento", "Numero de celular    (+258)", "Sexo", "Contacto de emergência", "Nome Completo", "Numero de Celular    (+258)", "nivel"};
 	String[] radio_text = new String[] {"M", "F"};
+	String[] nivel_text = {"Nivel 1", "Nivel 2", "Nivel 3","Nivel 4"};
 	
 	//Radio button
 	JRadioButton[] radio_button;
+	JRadioButton[] nivel_radio = new JRadioButton[nivel_text.length];
+	ButtonGroup nivel_g;
 	
 	//setting buttons
 	JButton submit = new JButton("Salvar");
@@ -99,20 +102,16 @@ public class CadastrarEstudante extends JPanel implements ActionListener{
 		JLabel label_title2 = new JLabel("informacoes de emergencia".toUpperCase());
 
 		for(int i = 0; i < form_text.length; i++){
-
+			System.out.println("i = " + i + " = " + form_text[i]);
 			
-			label[i] = new JLabel(form_text[i].toUpperCase());
+			label[i] = new JLabel(organize_string(form_text[i]).toUpperCase());
 			label[i].setFont(Style.tf_font);
 				
 			if(i != 6){
 				text_field[i] = new JTextField(25);
 				text_field[i].setFont(Style.tf_font);
-				
-				
 			}
 			else if( i == 6){
-
-
 
 				for(int k = 0; k < radio_text.length; k++){
 
@@ -124,8 +123,9 @@ public class CadastrarEstudante extends JPanel implements ActionListener{
 					radio_button[k].addActionListener(this);
 				}
 			}
+
 			
-			panel[i] = new JPanel(Style.flow_center);			
+			panel[i] = new JPanel(Style.flow_center);
 			panel[i].setBackground(Style.bg);
 			panel[i].add(label[i]);
 			
@@ -133,24 +133,56 @@ public class CadastrarEstudante extends JPanel implements ActionListener{
 				panel[i].add(text_field[i]);
 				
 			} catch(NullPointerException e){
-				JPanel jp = new JPanel();
-				jp.setBackground(Style.bg);
+				if(i == 6){
+					panel[i].add(radio_button[0]);
+					panel[i].add(radio_button[1]);
 
-				label_title2.setFont(new Font("Consolas", Font.BOLD, 22));
-				label_title2.setForeground(Style.fg);
+					ButtonGroup group = new ButtonGroup();
+					group.add(radio_button[0]);
+					group.add(radio_button[1]);
 
-				panel_title2.setBackground(Style.bg);
-				panel_title2.add(label_title2);
+					JPanel jp = new JPanel();
+					jp.setBackground(Style.bg);
+
+					label_title2.setFont(new Font("Consolas", Font.BOLD, 22));
+					label_title2.setForeground(Style.fg);
+
+					panel_title2.setBackground(Style.bg);
+					panel_title2.add(label_title2);
+					
+					this.add(jp);
+					this.add(panel_title2);
 				
-				this.add(jp);
-				this.add(panel_title2);
+				}
+			}
+
+			if(i == 10) {
+				System.out.println(i);
+				for(int j = 0; j < nivel_text.length; j++){
+					System.out.println(i);
+					nivel_radio[j] = new JRadioButton(nivel_text[j]);
+					nivel_radio[j].setBackground(Style.bg);
+					nivel_radio[j].setFocusable(false);
+					nivel_radio[j].setFont(Style.tf_font);
+					nivel_radio[j].setForeground(Style.fg);
+
+					nivel_radio[j].addActionListener(this);
+					
+				}
 				
-				panel[i].add(radio_button[0]);
-				panel[i].add(radio_button[1]);
+				panel[i].remove(text_field[i]);
+				panel[i].remove(label[i]);
 				
-				ButtonGroup group = new ButtonGroup();
-				group.add(radio_button[0]);
-				group.add(radio_button[1]);
+				JPanel pn = new JPanel();
+				pn.setBackground(Style.bg);
+				//this.add(pn);
+				
+				nivel_g = new ButtonGroup();
+				
+				for(int l = 0; l < nivel_text.length; l++){
+					nivel_g.add(nivel_radio[l]);
+					panel[i].add(nivel_radio[l]);
+				}
 			}
 			
 			panel[i].setPreferredSize(new Dimension(200, 50));
@@ -176,6 +208,19 @@ public class CadastrarEstudante extends JPanel implements ActionListener{
 		this.add(panels[10]);
 		
 	}
+
+	private String organize_string(String string){
+		String max = form_text[7];
+		int min_str = string.length(), max_str = max.length(), mid = max_str-min_str;
+
+		for(int i = 0; i <= mid; i++){
+
+			string = "  " + string;
+
+		}
+
+		return string;
+	}
 	
 	int status_nivel = 0;
 	public void actionPerformed(ActionEvent e){
@@ -198,9 +243,15 @@ public class CadastrarEstudante extends JPanel implements ActionListener{
 			String contacto_emergencia = text_fields[7].getText();
 			String nome_emergencia = text_fields[8].getText();
 			String nr_celular_emergencia = text_fields[9].getText();
-			String nivel = text_fields[10].getText();
+			String nivel = "";
 
-			Time horario = new Time(130000);
+			for(int i =0; i< nivel_text.length; i++){
+				if(nivel_radio[i].isSelected()){
+					nivel = nivel_radio[i].getText();
+				}
+			}
+
+			Time horario = new Time(120000);
 
 			java.util.Date date = new java.util.Date();
 			java.sql.Date data_matricula = new java.sql.Date(date.getTime());
@@ -213,7 +264,11 @@ public class CadastrarEstudante extends JPanel implements ActionListener{
 			}
 
 			String query = "INSERT INTO estudante (id_estudante, nome_completo, morada, bairro, email, data_nascimento, numero_celular, sexo, nivel, horario, data_matricula) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+			String emergencia_query = "INSERT INTO emergencia (id_estudante, contacto_emergencia, nome_completo, numero_celular) VALUES (?, ?, ?, ?)";
+
 			PreparedStatement ps = null;
+			PreparedStatement ps_em = null;
 
 			try{
 				ps = Conexao.getConexao_ees().prepareStatement(query);
@@ -231,17 +286,25 @@ public class CadastrarEstudante extends JPanel implements ActionListener{
 				ps.setDate(11, data_matricula);
 
 
+
+				ps_em = Conexao.getConexao_ees().prepareStatement(emergencia_query);
+				
+				ps_em.setString(1, id);
+				ps_em.setString(2, contacto_emergencia);
+				ps_em.setString(3, nome_emergencia);
+				ps_em.setString(4, "+258"+nr_celular_emergencia);
+
 				try {
-						if(ps.executeUpdate() == 1){
+						if(ps.executeUpdate() == 1 && ps_em.executeUpdate() == 1){
 							JOptionPane.showMessageDialog(Login.janela, "Estudante " + nome + " cadastrado!", "Sucesso!!!", JOptionPane.INFORMATION_MESSAGE);
 						}
-					// System.out.println(ps);
-					// System.out.println(query);
 
 				} catch (Exception ex) {
 					JOptionPane.showMessageDialog(null, ex, "Erro", JOptionPane.INFORMATION_MESSAGE);
 				}
+
 				ps.close();
+				ps_em.close();
 			}
 			catch(SQLException ex){
 				ex.printStackTrace();
