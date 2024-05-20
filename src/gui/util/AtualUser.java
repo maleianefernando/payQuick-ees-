7f3ef9882c8e23284c6408b3013fde50476a73ee
Fernando Maleiane <maleianefernando3@gmail.com>
2024-05-20 15:31:29 +0200

    a file that stores the credentials os the logged user

---
+++ src/gui/util/AtualUser.java
@@ -0,0 +1,16 @@
+package gui.util;
+
+public class AtualUser {
+    
+    public String name;
+    public String id;
+    public String password;
+    public String position;
+
+    public AtualUser(String name, String id, String password, String position){
+        this.name = name;
+        this.id = id;
+        this.position = position;
+        this.password = password;
+    }
+}
